d463c040f909014f78b39958729333c139a6fdd8
Hinn2605 <nhanhoai2605@gmail.com>
2022-12-13 21:21:23 +0700

    up

--- jvscript/header.js
+++ jvscript/header.js
@@ -4,17 +4,17 @@ const bgload = () =>{
     let line = document.getElementById("line");
 
     setTimeout(() => {
-        header.style.background = "url('img/img-header/header-1.jpg')no-repeat center center/cover";
+        header.style.background = "url('/img/img-header/header-1.jpg')no-repeat center center/cover";
         
         line.style.height = "33.3%";
     }, 0000);
     setTimeout(() => {
-        header.style.background = "url('img/img-header/header-2.jpg')no-repeat center center/cover";
+        header.style.background = "url('/img/img-header/header-2.jpg')no-repeat center center/cover";
         
         line.style.height = "66.3%";
     }, 3000);
     setTimeout(() => {
-        header.style.background = "url('img/img-header/header-3.jpg')no-repeat center center/cover";
+        header.style.background = "url('/img/img-header/header-3.jpg')no-repeat center center/cover";
         
         line.style.height = "99.3%";
     }, 6000);
